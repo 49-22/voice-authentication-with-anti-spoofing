11fd10ca8974137a57a3212af8ee1de12a5fe388
Nitin <nitin.nagamalla@gmail.com>
2025-07-05 10:50:13 +0000

    feat(speaker): extracts embedding for a speaker

---
+++ app/speaker_model.py
@@ -0,0 +1,13 @@
+from speechbrain.pretrained import SpeakerRecognition
+import torchaudio
+
+speaker_model = SpeakerRecognition.from_hparams(
+    source="speechbrain/spkrec-ecapa-voxceleb",
+    savedir = "models/ecapa"
+)
+
+def extract_embedding(audio_path):
+    signal, fs = torchaudio.load(audio_path)
+    # 512 dimentional vector for storing the speaker voice
+    embedding = speaker_model.encode_batch(signal).squeeze().detact().numpy()
+    return embedding
